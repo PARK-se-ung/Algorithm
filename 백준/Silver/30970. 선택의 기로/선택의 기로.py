496ea1f1eb11d1e039907e56058b3fdfa45bd09a
PARK-se-ung <seungbag3586@gmail.com>
2025-11-27 06:06:15 +0900

    [Silver IV] Title: 선택의 기로, Time: 540 ms, Memory: 131508 KB -BaekjoonHub

---
+++ 백준/Silver/30970. 선택의 기로/선택의 기로.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = [tuple(map(int, input().split())) for _ in range(n)]
+
+arr.sort(key = lambda x : (-x[0], x[1]))
+print(f"{' '.join(map(str, arr[0]))} {' '.join(map(str, arr[1]))}")
+
+arr.sort(key = lambda x : (x[1], -x[0]))
+print(f"{' '.join(map(str, arr[0]))} {' '.join(map(str, arr[1]))}")
