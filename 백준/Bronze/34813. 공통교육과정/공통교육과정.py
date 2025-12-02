d724c0baa5fce7b30ffd4a1ff4efe49e5170f05a
PARK-se-ung <seungbag3586@gmail.com>
2025-12-02 13:43:57 +0900

    [Bronze V] Title: 공통교육과정, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/34813. 공통교육과정/공통교육과정.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+s = input()[:1]
+
+if s == 'F': print('Foundation')
+elif s == 'C': print('Claves')
+elif s == 'V': print('Veritas')
+elif s == 'E': print('Exploration')
