e75c1cca9868357a92ce7d7ece6e91db6e52e889
PARK-se-ung <seungbag3586@gmail.com>
2025-12-10 16:05:49 +0900

    [Bronze III] Title: Viva la Diferencia, Time: 112 ms, Memory: 110912 KB -BaekjoonHub

---
+++ 백준/Bronze/4084. Viva la Diferencia/Viva la Diferencia.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+while True:
+    a, b, c, d = map(int, input().split())
+    if a == b == c == d == 0: break
+
+    result = 0
+    while not (a == b == c == d):
+        a, b, c, d = abs(a - b), abs(b - c), abs(c - d), abs(d - a)
+        result += 1
+    
+    print(result)
