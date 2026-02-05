3c4b379cc27aa4fdac09cbe56deccbcb6d98ab7d
PARK-se-ung <seungbag3586@gmail.com>
2026-02-05 14:57:06 +0900

    [Platinum II] Title: N-Queen 2, Time: 88 ms, Memory: 109544 KB, Score: 500 point -BaekjoonHub

--- 백준/Platinum/21133. N－Queen 2/N－Queen 2.py
+++ 백준/Platinum/21133. N－Queen 2/N－Queen 2.py
@@ -4,7 +4,24 @@ input = sys.stdin.readline
 n = int(input())
 
 arr = []
-if n % 2 == 0:
+
+if n % 6 == 2:
+    for i in range(n // 2):
+        arr.append(2 * i + 2)
+    arr.append(3)
+    arr.append(1)
+    for i in range(n // 2 - 3):
+        arr.append(2 * i + 7)
+    arr.append(5)
+elif n % 6 == 3:
+    for i in range(n // 2 - 1):
+        arr.append(2 * i + 4)
+    arr.append(2)
+    for i in range(n // 2 - 1):
+        arr.append(2 * i + 5)
+    arr.append(1)
+    arr.append(3)
+elif n % 2 == 0:
     for i in range(n):
         if i < n // 2:
             arr.append(2 * i + 2)
