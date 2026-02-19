51028310059121e9c578ec3591f9d5fb1d031153
PARK-se-ung <seungbag3586@gmail.com>
2026-02-19 18:03:55 +0900

    [Bronze III] Title: 아 맞다 마늘, Time: 76 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/32978. 아 맞다 마늘/아 맞다 마늘.py
+++ 백준/Bronze/32978. 아 맞다 마늘/아 맞다 마늘.py
@@ -2,13 +2,9 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-temp = list(input().split())
-origin = {temp[i]:0 for i in range(n)}
-ingredient = list(input().split())
-for elem in ingredient:
-    origin[elem] = 1
-
-for elem in temp:
-    if origin[elem] == 0:
+origin = list(input().split())
+ingredient = set(input().split())
+for elem in origin:
+    if elem not in ingredient:
         print(elem)
         break
