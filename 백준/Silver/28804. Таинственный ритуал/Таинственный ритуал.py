2b2f155ac397ccc6ea72e141491b96d813ec8e80
PARK-se-ung <seungbag3586@gmail.com>
2026-02-25 06:48:36 +0900

    [Silver III] Title: Таинственный ритуал, Time: 208 ms, Memory: 175864 KB -BaekjoonHub

--- 백준/Silver/28804. Таинственный ритуал/Таинственный ритуал.py
+++ 백준/Silver/28804. Таинственный ритуал/Таинственный ритуал.py
@@ -12,15 +12,14 @@ def func(x):
     if x == 8:
         return 4
     if x > 10:
-        return func(1 + func(x % 10))
+        return 1 + func(x % 10)
     return x
 
 for _ in range(int(input())):
     arr = list(map(int, list(input().rstrip())))    
     while len(arr) > 1:
         last = arr.pop()
-        arr[-1] += last // 10
-        arr[-1] += func(last % 10)
+        arr[-1] += func(last)
     
     while arr[0] not in {0, 1, 3, 7, 9}:
         arr[0] = func(arr[0])
