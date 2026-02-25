92c6f47541d4c44399d8c4a3352755f1f2d1cab7
PARK-se-ung <seungbag3586@gmail.com>
2026-02-26 06:18:03 +0900

    [Bronze V] Title: Киноманы, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/28927. Киноманы/Киноманы.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+t1, e1, f1 = map(int, input().split())
+t2, e2, f2 = map(int, input().split())
+a, b = 3 * t1 + 20 * e1 + 120 * f1, 3 * t2 + 20 * e2 + 120 * f2
+
+if a > b: print("Max")
+elif a == b: print("Draw")
+else: print("Mel")
