6e825823533fd4a6218ba90cdfc4433c807766f3
PARK-se-ung <seungbag3586@gmail.com>
2026-04-03 07:05:24 +0900

    [Gold V] Title: Subsequences Summing to Sevens, Time: 104 ms, Memory: 112872 KB -BaekjoonHub

--- 백준/Gold/11974. Subsequences Summing to Sevens/Subsequences Summing to Sevens.py
+++ 백준/Gold/11974. Subsequences Summing to Sevens/Subsequences Summing to Sevens.py
@@ -7,15 +7,9 @@ for i in range(n):
     prefix.append((prefix[-1] + int(input())) % 7)
 
 M = 0
-memo = [[] for _ in range(7)]
+memo = [-1] * 7
 for i in range(n + 1):
-    if prefix[i] == 0: M = max(M, i)
-    else:
-        if len(memo[prefix[i]]) >= 2: memo[prefix[i]].pop()
-        memo[prefix[i]].append(i)
-
-for i in range(1, 7):
-    if len(memo[i]) == 2:
-        M = max(M, memo[i][1] - memo[i][0])
+    if memo[prefix[i]] == -1: memo[prefix[i]] = i
+    else: M = max(M, i - memo[prefix[i]])
 
 print(M)
